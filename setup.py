3b043aa3e5bfbfd300bbf521848dfc7f229f8d05
Gonzalo Goral <goral.gonzalo@gmail.com>
2019-05-15 16:49:58 -0300

    Update setup.py
    * Arreglado el versionado semántico https://semver.org/
    * Enlace en la url

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='strategy',
-    version='0.1',
+    version='0.1.0',
     packages=['strategy'],
-    url='',
+    url='https://github.com/JonathanLoscalzo/strategy-external',
     license='MIT',
     author='jloscalzo',
     author_email='jonathan.r.loscalzo@gmail.com',
